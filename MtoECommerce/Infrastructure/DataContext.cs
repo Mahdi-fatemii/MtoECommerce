36f57b0b306ddf0aac6c77e377862ee8df988aef
Mahdi Fatemiy Kia <136173164+Mahdi-fatemii@users.noreply.github.com>
2025-05-23 13:50:21 +0330

    SeedPages

--- MtoECommerce/Infrastructure/DataContext.cs
+++ MtoECommerce/Infrastructure/DataContext.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MtoECommerce.Controllers;
 
 namespace MtoECommerce.Infrastructure
 {
     public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
         public DbSet<Page> Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Page>().HasData(
+                new Page { Id = 1, Title = "Home", Slug = "home", Body = "This is the home page" },
+                new Page { Id = 2, Title = "About", Slug = "about", Body = "This is the about page" },
+                new Page { Id = 3, Title = "Services", Slug = "services", Body = "This is the services page" },
+                new Page { Id = 4, Title = "Contact", Slug = "contact", Body = "This is the contact page" }
+                );
+        }
     }
 }
