a367eaca61184e6e42dae6398673b1bbf13b083f
Mahdi Fatemiy Kia <136173164+Mahdi-fatemii@users.noreply.github.com>
2025-05-25 18:16:26 +0330

    Admin Pages Controller

---
+++ MtoECommerce/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MtoECommerce.Infrastructure;
+
+namespace MtoECommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController(DataContext context) : Controller
+    {
+        private readonly DataContext _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            List<Page> pages = await _context.Pages.ToListAsync();
+
+            return View(pages);
+        }
+    }
+}
