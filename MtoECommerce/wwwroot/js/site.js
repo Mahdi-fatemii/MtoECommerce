1409d6944732d559813416bd4f60f6bff235a24d
Mahdi Fatemiy Kia <136173164+Mahdi-fatemii@users.noreply.github.com>
2025-07-07 14:24:41 +0330

    Partial notifications

--- MtoECommerce/wwwroot/js/site.js
+++ MtoECommerce/wwwroot/js/site.js
@@ -1,7 +1,13 @@
 ﻿$(function () {
 
-    $('a.confirm').on("click", function () {
+    $("a.confirm").on("click", function () {
         if (!confirm("Are you sure?")) return false;
     });
+
+    if ($("div.alert").length) {
+        setTimeout(() => {
+            $("div.alert").fadeOut();
+        }, 2000);
+    }
 
 });
