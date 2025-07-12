518f031b4744c63aaf8402beaf917e0be0af49ca
Mahdi Fatemiy Kia <136173164+Mahdi-fatemii@users.noreply.github.com>
2025-07-12 16:08:22 +0330

    Admin Create Product (POST)

--- MtoECommerce/Areas/Admin/Controllers/ProductsController.cs
+++ MtoECommerce/Areas/Admin/Controllers/ProductsController.cs
@@ -7,9 +7,11 @@ using MtoECommerce.Infrastructure;
 namespace MtoECommerce.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class ProductsController(DataContext context) : Controller
+    public class ProductsController(DataContext context,
+        IWebHostEnvironment webHostEnvironment) : Controller
     {
         private readonly DataContext _context = context;
+        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
 
         public async Task<IActionResult> Index()
         {
@@ -23,6 +25,57 @@ namespace MtoECommerce.Areas.Admin.Controllers
             ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await _context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "That product is already exists!");
+                    return View(product);
+                }
+
+                string imageName;
+
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
+
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+
+                    string filePath = Path.Combine(uploadsDir, imageName);
+
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+                else
+                {
+                    imageName = "noimage.png";
+                }
+
+                product.Image = imageName;
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
         }
     }
 }
